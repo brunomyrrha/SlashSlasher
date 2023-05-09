5229ceee86360cf682986338f7de55eb03f64304
Bruno Myrrha <brunomyrrha@icloud.com>
2023-05-09 23:43:56 +0200

    Initial Commit

---
+++ SlashSlasher/SlashSlasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlashSlasherApp.swift
+//  SlashSlasher
+//
+//  Created by Bruno Diniz on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlashSlasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
